4f7fef7dababfa4dafa3020cf14d52c775bd9a82
Paul <episodex@gmail.com>
2018-10-21 12:53:28 +0300

    Version 1.1 - Features & Fixes
    * Added badge widget.
    * Added `product_id` arguments to main widget and bottomline widget.
    * Now returning HTML instead of echoing yotpo functions.
    * Make sure to return nothing if post is not a product and no ID is supplied.

--- readme.txt
+++ readme.txt
@@ -11,9 +11,11 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 This plugin adds the ability to use shortcodes to control the placement of Yotpo widgets.
 
 == Description ==
-
+= Prerequisites (Important) =
+* The Yotpo plugin must be installed (obviously).
+* WooCommerce 3.X and above (due to new methods).
 This plugin allows using shortcodes to display Yotpo widgets inside and oustide (applicable widgets only) of product pages e.g. page builders, sidebars, widgets etc.
-Here is an example of the shortcodes provided:
+= Usage =
 ```
 [yotpo_widget]
 [yotpo_bottomline]
@@ -26,29 +28,29 @@ Special arguments exclusive to this plugin:
 2. **yotpo_product_reviews_carousel**:
 adding `product_id="product-id-here"` will display a reviews carousel for the given product id.
 adding `noproduct` will prevent a product id from being added in case you need a reviews carousel for all reviews.
+3. **yotpo_widget** and **yotpo_bottomline** accept an optional `product_id` argument (e.g. `product_id="47") if you'd like to provide a product ID. Otherwise, the ID of the current product will be used.
+
+For updates & stuff: http://paulglushak.com/yotpo-shortcodes-for-woocommerce/
 
 == Installation ==
-
 Installation is very simple:
 1. Upload the plugin files to the `/wp-content/plugins/yotpo-shortcodes` directory, or install the plugin through the WordPress plugins screen directly.
 2. Activate the plugin through the 'Plugins' screen in WordPress
 
-
 == Frequently Asked Questions ==
-
 = A shortcode is missing! / I need more shortcodes! =
-
 Get in touch, and I'll see what can be done.
 
 == Screenshots ==
-
 1. Example usage of the shortcodes.
-
 == Changelog ==
-
+= 1.1 =
+* Added badge widget.
+* Added optional `product_id` arguments to main widget and bottomline widget.
+* Now returning HTML instead of echoing yotpo functions.
+* Make sure to return nothing if post is not a product and no ID is supplied.
 = 1.0.1 =
 * Added `noproduct` arguments.
 * Initial release on WP.
-
 = 1.0 =
 * Initial commit.
--- wc_yotpo_shortcodes.php
+++ wc_yotpo_shortcodes.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name: Shortcodes for Yotpo
 * Description: This plugin adds the ability to use shortcodes to control the placement of Yotpo widgets.
-* Version: 1.0.1
+* Version: 1.1
 * Author: Paul Glushak
 * Author URI: http://paulglushak.com/
 * Plugin URI: http://paulglushak.com/yotpo-shortcodes-for-woocommerce/
@@ -11,23 +11,37 @@
 /*
  * This plugin allows using shortcodes to display Yotpo widgets inside and oustide (applicable widgets only) of product pages e.g. page builders, sidebars, widgets etc.
  * See example usage at the bottom.
- * 
 */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 function basic_check() { if ( !class_exists( 'woocommerce' ) && !function_exists( 'wc_yotpo_get_product_data' ) ) { return; } }
 
-function wc_place_yotpo_widget() {
+function wc_place_yotpo_widget( $args ) {
 	basic_check();
-	if ( is_product() ) { $html = wc_yotpo_show_widget(); }
-	echo $html;
+	if ( isset( $args['product_id'] ) ) { $product_id = $args['product_id']; } elseif ( is_product() ) { global $product; $product_id = $product->get_id(); } else { return; }
+	$widget_product_data = wc_yotpo_get_product_data( wc_get_product( $product_id ) );
+	$html = "<div class='yotpo yotpo-main-widget'
+   				data-product-id='". $product_id ."'
+   				data-name='". $widget_product_data['title'] ."' 
+   				data-url='". $widget_product_data['url'] ."' 
+   				data-image-url='". $widget_product_data['image-url'] ."' 
+  				data-description='". $widget_product_data['description'] ."' 
+  				data-lang='". $widget_product_data['lang'] ."'
+                data-price='". $product->get_price() ."'
+                data-currency='". get_woocommerce_currency() ."'></div>";
+	return $html;
 }
 
-function wc_place_yotpo_bottomline() {
-	basic_check();
-	if ( is_product() ) { $html = wc_yotpo_show_buttomline(); }
-	echo $html;
+function wc_place_yotpo_bottomline( $args ) {
+	basic_check();	
+	if ( isset( $args['product_id'] ) ) { $product_id = $args['product_id']; } elseif ( is_product() ) { global $product; $product_id = $product->get_id(); } else { return; }
+	$widget_product_data = wc_yotpo_get_product_data( wc_get_product( $product_id ) );
+	$html = "<div class='yotpo bottomLine'
+				data-product-id='". $product_id ."'
+   				data-url='". $widget_product_data['url'] ."' 
+   				data-lang='". $widget_product_data['lang'] ."'></div>";
+	return $html;
 }
 
 function wc_place_yotpo_product_gallery( $args ) {
@@ -69,14 +83,22 @@ function wc_place_yotpo_product_reviews_carousel( $args ) {
 	} elseif ( is_product() ) {
 		global $product;
 		$html .= 'data-product-id="' . $product->get_id() . '"';
+	} else {
+		return;
 	}
 	$html .= '></div>';
 	return $html;
 }
 
-add_shortcode( 'yotpo_widget', 'wc_place_yotpo_widget' ); # [yotpo_widget]
-add_shortcode( 'yotpo_bottomline', 'wc_place_yotpo_bottomline' ); # [yotpo_bottomline]
+function wc_place_yotpo_badge() {
+	$html = "<div id='y-badges' class='yotpo yotpo-badge badge-init'>&nbsp;</div>";
+	return $html;
+}
+
+add_shortcode( 'yotpo_widget', 'wc_place_yotpo_widget' ); # [yotpo_widget] with optional product_id argument e.g [yotpo_widget product_id="47"] 
+add_shortcode( 'yotpo_bottomline', 'wc_place_yotpo_bottomline' ); # [yotpo_bottomline] with optional product_id argument e.g [yotpo_widget product_id="47"] 
 add_shortcode( 'yotpo_product_gallery', 'wc_place_yotpo_product_gallery' ); # [yotpo_product_gallery gallery_id="5bbb561f5ea79223a9da0e7c"]
 add_shortcode( 'yotpo_product_reviews_carousel', 'wc_place_yotpo_product_reviews_carousel' ); # [yotpo_product_reviews_carousel background_color="#22fff2" mode="most-recent" type="both" count="3"]
+add_shortcode( 'yotpo_badge', 'wc_place_yotpo_badge' ); # [yotpo_badge]
 
 ?>
